224ce1f7d0a6fda5d7f3ec497b20816a9efd1ba0
Mac <380753814@qq.com>
2026-08-25 09:47:05 +0800

    refactor(system-service): use MapStruct converter

--- services/system-service/src/main/java/com/hl/platform/system/service/impl/FunctionServiceImpl.java
+++ services/system-service/src/main/java/com/hl/platform/system/service/impl/FunctionServiceImpl.java
@@ -1,6 +1,6 @@
 package com.hl.platform.system.service.impl;
 
-import com.hl.platform.system.entity.Function;
+import com.hl.platform.system.converter.FunctionConverter;
 import com.hl.platform.system.mapper.FunctionMapper;
 import com.hl.platform.system.service.FunctionService;
 import com.hl.platform.system.vo.FunctionVO;
@@ -18,9 +18,11 @@ import static com.hl.platform.system.entity.table.FunctionTableDef.FUNCTION;
 public class FunctionServiceImpl implements FunctionService {
 
     private final FunctionMapper functionMapper;
+    private final FunctionConverter functionConverter;
 
-    public FunctionServiceImpl(FunctionMapper functionMapper) {
+    public FunctionServiceImpl(FunctionMapper functionMapper, FunctionConverter functionConverter) {
         this.functionMapper = functionMapper;
+        this.functionConverter = functionConverter;
     }
 
     @Override
@@ -29,20 +31,6 @@ public class FunctionServiceImpl implements FunctionService {
                 .where(FUNCTION.PARENTID.eq(parentId))
                 .orderBy(FUNCTION.ORDERNUM.asc());
 
-        return functionMapper.selectListByQuery(queryWrapper).stream()
-                .map(this::toVO)
-                .toList();
-    }
-
-    private FunctionVO toVO(Function function) {
-        FunctionVO functionVO = new FunctionVO();
-        functionVO.setFuncId(function.getFuncid());
-        functionVO.setParentId(function.getParentid());
-        functionVO.setFuncName(function.getFuncname());
-        functionVO.setFuncTitle(function.getFunctitle());
-        functionVO.setFuncUrl(function.getFuncurl());
-        functionVO.setFuncType(function.getFunctype());
-        functionVO.setOrderNum(function.getOrdernum());
-        return functionVO;
+        return functionConverter.toVOList(functionMapper.selectListByQuery(queryWrapper));
     }
 }
