af60e58e293d2ae16ee8b61e17d70a8d9b40a84e
Mac <380753814@qq.com>
2026-08-31 09:27:07 +0800

    feat(base): define trusted authentication header names

---
+++ platform-base/src/main/java/com/hl/platform/base/security/AuthHeaders.java
@@ -0,0 +1,11 @@
+package com.hl.platform.base.security;
+
+public final class AuthHeaders {
+
+    public static final String USER_ID = "X-Auth-User-Id";
+    public static final String SID = "X-Auth-Sid";
+    public static final String TOKEN_VERSION = "X-Auth-Token-Version";
+
+    private AuthHeaders() {
+    }
+}
